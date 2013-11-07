9c2a11ab22462a848294c4f8f897970301120530
Mirza Shah <mas644@gmail.com>
2013-11-06 23:15:42 -0800

    First go of complete pipeline for 2 arm scanner

--- storybox_feeder_mas/storybox_feeder_mas.ino
+++ storybox_feeder_mas/storybox_feeder_mas.ino
@@ -11,12 +11,13 @@
 #define TRAY_MOTOR_STEP_PIN             7    // Tray motor step pin
 #define TRAY_MOTOR_DIR_PIN              8    // Tray motor dir pin
 
-#define CAMERA_SHOOT_PIN                5    // Pin that triggers camera switch
+#define CAMERA_SHOOT_PIN                4    // Pin that triggers camera switch
 
 #define SCANNER1_SHOOT_PIN              5    // Pin that triggers scanner 1
 #define SCANNER2_SHOOT_PIN              6    // Pin that triggers scanner 2
 
-#define VACUUM_PUMP_PIN                 500  // Pin for the vacuum pump that sucks up photos
+#define VACUUM_PUMP1_PIN                3    // Pin for the vacuum pump 1 that sucks up photos
+#define VACUUM_PUMP2_PIN                11   // Pin for the vacuum pump 2 that sucks up photos
 
 #define PHOTOCELL_PIN                   A0   // The photocell with a laser being shot at it
 #define PHOTOCELL_OBFUSCATION_THRESHOLD 850  // The threshold for laser which triggers scanner
@@ -30,8 +31,6 @@
 #define FEEDER_ARM_DISCARD_ANGLE        95
 #define FEEDER_ARM_DISCARD_DELAY        3000
 
-  
-//#define VACUUM_PIN                      ?
 
 class ArmServo
 {
@@ -39,11 +38,14 @@ class ArmServo
     ArmServo(int servoPin)
     /*****************************************************************************/
     {
-      _servo.attach(servoPin);
-      _servoPin = servoPin;
-      _position = _servo.read();
+      _servoPin = servoPin; 
     }
-
+    
+    void init()
+    {
+      _servo.attach(_servoPin);
+    }
+    
     int getPosition() const 
     /*****************************************************************************/
     {
@@ -59,9 +61,9 @@ class ArmServo
         if(_position == angleInDegrees)
           break;
         _position += increment;
-        Serial.print("Servo ");
-        Serial.print(_servoPin);
-        Serial.println(_position);
+        //Serial.print("Servo ");
+        //Serial.print(_servoPin);
+        //Serial.println(_position);
         _servo.write(_position);
         delay(delayBetweenStepsInMilliseconds);
       }
@@ -81,7 +83,7 @@ class Tray
     {
       _motorStepPin = trayMotorStepPin;
       _motorDirectionPin = trayMotorDirectionPin;
-      pinMode(_motorDirectionPin, OUTPUT);     
+      pinMode(_motorStepPin, OUTPUT);     
       pinMode(_motorDirectionPin, OUTPUT);
       digitalWrite(_motorStepPin,LOW);
     }
@@ -178,18 +180,19 @@ class VacuumPump
     /*****************************************************************************/
     {
       _vacuumPin = vacuumPin;
+      pinMode(vacuumPin, OUTPUT);
     }
 
     void turnOn()
     /*****************************************************************************/
     {
-      //Complete
+      analogWrite(_vacuumPin, 255);
     }
 
     void turnOff()
     /*****************************************************************************/
     {
-      //Complete
+      analogWrite(_vacuumPin, 0);
     }
   
   private:
@@ -207,39 +210,92 @@ Tray             photoTray(TRAY_MOTOR_STEP_PIN, TRAY_MOTOR_DIR_PIN);
 ImageInputDevice camera(CAMERA_SHOOT_PIN);
 ImageInputDevice scanner1(SCANNER1_SHOOT_PIN);
 ImageInputDevice scanner2(SCANNER2_SHOOT_PIN);
-VacuumPump       vacuum(VACUUM_PUMP_PIN);
+VacuumPump       vacuum1(VACUUM_PUMP1_PIN);
+VacuumPump       vacuum2(VACUUM_PUMP2_PIN);
 
 void setup()
 /*****************************************************************************/
 {
   //Initialize serial comms for debugging
   Serial.begin(9600);
+  
+  //Initialize arms
+  arm1.init();
+  arm2.init();
 
   //Bring feeder arms to center
   arm1.moveTo(FEEDER_ARM_DISCARD_ANGLE);
   arm2.moveTo(FEEDER_ARM_DISCARD_ANGLE);
+  
+  vacuum1.turnOff();
+  vacuum2.turnOff();
 }
 
 void loop()
 /*****************************************************************************/
 {
+  //Turn on the vacuum
+  Serial.println("Turn on vacuum 1");
+  vacuum1.turnOn();
+  
   //Get next photo ready 
+  Serial.println("Adjust photo tray (arm 1 is next receiver).");
   photoTray.calibrate();
 
   //Take a photo of the picture
+  Serial.println("Take picture of back of photo (for arm1).");
   camera.takePicture();
 
   //Move the arm to pickup picture
+  Serial.println("Moving arm 1 to pickup photo.");  
   arm1.moveTo(FEEDER_ARM_PICKUP_ANGLE);
   delay(1000);
 
-  //Move the arm to pickup
-  arm1.moveTo(FEEDER_ARM_PICKUP_ANGLE);
+  //Move the arm to the scanner and trigger the scanner
+  Serial.println("Moving arm 1 to scanning position.");
+  arm1.moveTo(FEEDER_ARM_SCAN_ANGLE);
+  Serial.println("Starting scan on scanner 1.");
   scanner1.takePicture();
-  delay(50000);
+  
+  //While we're scanning on photo scanner 1, let's take a picture of next photo and get arm 2 ready
+  Serial.println("Adjust photo tray (arm 2 is next receiver).");
+  photoTray.calibrate();
+  Serial.println("Take picture of back of photo (for arm2).");
+  camera.takePicture();
+  Serial.println("Turn on vacuum 2.");
+  vacuum2.turnOn();
+  Serial.println("Moving arm 2 to pickup photo.");
+  arm2.moveTo(FEEDER_ARM_PICKUP_ANGLE);
+  Serial.println("Wait for arm 2 to suckup photo.");
+  delay(1000);
+  Serial.println("Moving arm 2 to scanning position.");
+  arm2.moveTo(FEEDER_ARM_SCAN_ANGLE);
+  Serial.println("Starting scan on scanner 2.");
+  scanner2.takePicture();
+  
+  //Wait for scanner 1 to finish
+  Serial.println("Waiting out remaining time for scan 1");
+  delay(10000);
 
+  //Move arm1 to the discard position, disable the vacuum
+  Serial.println("Assuming scan on scanner 1 complete, moving arm 1 to discard position.");
   arm1.moveTo(FEEDER_ARM_DISCARD_ANGLE);
-  vacuum.turnOff();
-  delay(FEEDER_ARM_DISCARD_DELAY);
+  
+  Serial.println("Turn off vacuum 1.");
+  vacuum1.turnOff();
+  
+  Serial.println("Waiting a sec for photo to fall off arm 1.");
+  delay(FEEDER_ARM_DISCARD_DELAY);  
+  
+  //Wait for scanner 2 to finish
+  Serial.println("Waiting out remaining time for scan 2");
+  delay(10000);
+  
+  //Move arm2 to the discard position, disable the vacuum
+  Serial.println("Assuming scan on scanner 2 complete, moving arm 2 to discard position.");
+  arm2.moveTo(FEEDER_ARM_DISCARD_ANGLE);
+  Serial.println("Turn off vacuum 2.");
+  vacuum2.turnOff();
+  Serial.println("Waiting a sec for photo to fall off arm 2.");
+  delay(FEEDER_ARM_DISCARD_DELAY);   
  }
-
